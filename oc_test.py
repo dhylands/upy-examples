b4b2ad2831c272d0dea44e465fad63ef6432f241
Dave Hylands <dhylands@gmail.com>
2014-09-14 20:16:00 -0700

    Added oc_test logic analyzer output.

--- oc_test.py
+++ oc_test.py
@@ -1,9 +1,11 @@
 import pyb
-import micropython
 
-debug_pin = pyb.Pin('X1', pyb.Pin.OUT_PP)
+# setup oc_clock to be a 1 kHz clock. Since it toggles we want it to toggle
+# 2000 times per second to get a 1000 Hz clock.
+t2 = pyb.Timer(2, freq=2000)
+oc  = t2.channel(2, mode=pyb.Timer.OC, pin=pyb.Pin.board.X2, oc_mode=pyb.Timer.OC_MODE_TOGGLE)
 
-# setup oc_clock to be a 1 kHz clock
-oc  = pyb.Timer(2, pin=pyb.Pin.board.X2, mode=pyb.Timer.OC,  channel=2, oc_mode=pyb.Timer.OC_MODE_TOGGLE, freq=2000)
-pwm = pyb.Timer(5, pin=pyb.Pin.board.X3, mode=pyb.Timer.PWM, channel=3, prescaler=41999, period=9, pulse=1)
+# stup PWM to be 200 Hz with a 1 clock pulse_width
+t5 = pyb.Timer(5, prescaler=41999, period=9)
+pwm = t5.channel(3, mode=pyb.Timer.PWM, pin=pyb.Pin.board.X3, pulse_width=1)
 
