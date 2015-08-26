a35329de7446f399e455230bf3185edf9531b94d
Dave Hylands <dhylands@gmail.com>
2015-08-26 08:56:55 -0700

    Added pyb.usb_mode call

--- boot.py
+++ boot.py
@@ -6,7 +6,7 @@ import pyb
 import micropython
 import sys
 #pyb.main('main.py') # main script to run after this one
-#pyb.usb_mode('CDC+MSC') # act as a serial and a storage device
+pyb.usb_mode('CDC') # act as a serial and a storage device
 #pyb.usb_mode('CDC+HID') # act as a serial device and a mouse
 
 def bl():
@@ -23,7 +23,7 @@ def af():
         print('{:10s} {:s}'.format(pin_name, str(pin.af_list())))
 
 def init():
-    if False:
+    if True:
         uart = pyb.UART(6,115200)
         pyb.repl_uart(uart)
         print("REPL is also on UART 6 (Y1=Tx Y2=Rx)")
