4344a8c426ff77be2ad50272ce7b0e0251291793
Dave Hylands <dhylands@gmail.com>
2015-11-04 09:10:49 -0800

    Added various boot.py examples

---
+++ boot-cdc-only.py
@@ -0,0 +1,39 @@
+# boot.py -- run on boot-up
+#
+# This is some common initialization that I like to keep around.
+
+import pyb
+import micropython
+import sys
+#pyb.main('main.py') # main script to run after this one
+before_mode = pyb.usb_mode()
+pyb.usb_mode('CDC') # act as a serial only
+after_mode = pyb.usb_mode()
+
+#pyb.usb_mode('CDC+MSC') # act as a serial and a storage device
+#pyb.usb_mode('CDC+HID') # act as a serial device and a mouse
+
+def bl():
+    pyb.bootloader()
+
+def pins():
+    for pin_name in dir(pyb.Pin.board):
+        pin = pyb.Pin(pin_name)
+        print('{:10s} {:s}'.format(pin_name, str(pin)))
+
+def af():
+    for pin_name in dir(pyb.Pin.board):
+        pin = pyb.Pin(pin_name)
+        print('{:10s} {:s}'.format(pin_name, str(pin.af_list())))
+
+def init():
+    if False:
+        uart = pyb.UART(6,115200)
+        pyb.repl_uart(uart)
+        print("REPL is also on UART 6 (Y1=Tx Y2=Rx)")
+    if False:
+        bufsize = 100
+        print("Setting alloc_emergency_exception_buf to",  bufsize)
+        micropython.alloc_emergency_exception_buf(bufsize)
+
+init()
