6fec50cddb109d4cb7bd92b3ad3b15409b8d8dee
Dave Hylands <dhylands@gmail.com>
2017-03-20 08:42:35 -0700

    Added nvic.py

---
+++ nvic.py
@@ -0,0 +1,26 @@
+import machine
+
+SCS = 0xE000E000
+SCB = SCS + 0x0D00
+NVIC = SCS + 0x0100
+VTOR = SCB + 0x08
+
+SCB_SHP = SCB + 0x18
+NVIC_PRIO = NVIC + 0x300
+
+print('NVIC_PRIO = {:08x} @ {:08x}'.format(machine.mem32[NVIC_PRIO], NVIC_PRIO))
+print('VTOR      = {:08x} @ {:08x}'.format(machine.mem32[VTOR], VTOR))
+
+print('System IRQs')
+for i in range(12):
+    irq = -(16 - (i + 4))
+    prio = machine.mem8[SCB_SHP + i] >> 4
+    if prio > 0:
+        print('{:3d}:{:d}'.format(irq, prio))
+
+print('Regular IRQs')
+for irq in range(80):
+    prio = machine.mem8[NVIC_PRIO + irq] >> 4
+    if prio > 0:
+        print('{:3d}:{:d}'.format(irq, prio))
+
